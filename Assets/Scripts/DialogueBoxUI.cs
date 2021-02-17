610b0da5f67bd64476abb2babc5d6a60679c2911
Evan Griffiths <evangriffiths@hotmail.co.uk>
2021-02-17 12:13:42 +0000

    ✨ Initial dialogue system working

---
+++ Assets/Scripts/DialogueBoxUI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogueBoxUI : MonoBehaviour
+{
+    [SerializeField] private TMP_Text NameText;
+    [SerializeField] private TMP_Text DialogueText;
+
+    private void Start()
+    {
+        Hide();
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+    
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+    
+    public void SetName(string name)
+    {
+        NameText.text = name;
+    }
+
+    public void SetDialogue(string dialogue)
+    {
+        DialogueText.text = dialogue;
+    }
+}
---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public static DialogueManager Instance;
+
+    [SerializeField] private DialogueBoxUI UI;
+    
+    private Queue<string> sentences;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        Debug.Log($"Starting conversation with {dialogue.Name}");
+        UI.SetName(dialogue.Name);
+        
+        sentences.Clear();
+
+        for (var index = 0; index < dialogue.Sentences.Length; index++)
+        {
+            var sentence = dialogue.Sentences[index];
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+        
+        UI.Show();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count <= 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        var sentence = sentences.Dequeue();
+        UI.SetDialogue(sentence);
+    }
+
+    void EndDialogue()
+    {
+        Debug.Log("End of conversation");
+        UI.Hide();
+    }
+}
